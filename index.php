24d2507c5dfa256e2c184b44ded2582b5204692f
Tim Geiser <timg1234@gmail.com>
2023-09-08 21:03:53 -0500

    + php

--- index.php
+++ index.php
@@ -3,47 +3,78 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>kW, HP, Btu/h & Ton Converter</title>
+    <title>Units Converter</title>
     <link rel="stylesheet" href="styles.css" />
   </head>
   <body>
     <div class="converter">
-      <span class="power-label">Power</span>
+      <span class="label">Power</span>
 
       <input
         type="number"
         id="kWInput"
-        placeholder="Enter value"
+        placeholder="Enter #"
         onkeyup="convert('kW')"
       />
-      <label for="kWInput">kW</label>
-      <p>=</p>
+      <label for="kWInput" class="unit-label">kW</label>
 
       <input
         type="number"
         id="HPInput"
-        placeholder="Enter value"
+        placeholder="Enter #"
         onkeyup="convert('HP')"
       />
-      <label for="HPInput">HP</label>
-      <p>=</p>
+      <label for="HPInput" class="unit-label">HP</label>
 
       <input
         type="number"
         id="BtuInput"
-        placeholder="Enter value"
+        placeholder="Enter #"
         onkeyup="convert('Btu')"
       />
-      <label for="BtuInput">Btu/h</label>
-      <p>=</p>
+      <label for="BtuInput" class="unit-label">Btu/h</label>
 
       <input
         type="number"
         id="tonInput"
-        placeholder="Enter value"
+        placeholder="Enter #"
         onkeyup="convert('Ton')"
       />
-      <label for="tonInput">Tons</label>
+      <label for="tonInput" class="unit-label">Tons</label>
+
+      <span class="label">Temperature</span>
+
+      <input
+        type="number"
+        id="CInput"
+        placeholder="Enter #"
+        onkeyup="convert('C')"
+      />
+      <label for="CInput" class="unit-label">C</label>
+
+      <input
+        type="number"
+        id="FInput"
+        placeholder="Enter #"
+        onkeyup="convert('F')"
+      />
+      <label for="FInput" class="unit-label">F</label>
+
+      <input
+        type="number"
+        id="RInput"
+        placeholder="Enter #"
+        onkeyup="convert('R')"
+      />
+      <label for="RInput" class="unit-label">R</label>
+
+      <input
+        type="number"
+        id="KInput"
+        placeholder="Enter #"
+        onkeyup="convert('K')"
+      />
+      <label for="KInput" class="unit-label">K</label>
     </div>
     <script src="script.js"></script>
   </body>
--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+/**
+ * Convert values based on the input type.
+ * @param {string} type - The type of the input (kW, HP, Btu, Ton, C, F, R, K).
+ */
 function convert(type) {
+  // Constants for power conversions
   const kWtoBtu = 3412.14;
   const kWtoTon = 0.284345;
   const kWtoHP = 1.34102;
+
+  // Functions for temperature conversions
+  const CtoF = (c) => (c * 9) / 5 + 32;
+  const CtoR = (c) => ((c + 273.15) * 9) / 5;
+  const CtoK = (c) => c + 273.15;
+  const FtoC = (f) => ((f - 32) * 5) / 9;
+  const RtoC = (r) => ((r - 491.67) * 5) / 9;
+  const KtoC = (k) => k - 273.15;
 
   switch (type) {
     case "kW":
@@ -18,36 +31,7 @@ function convert(type) {
         );
       }
       break;
-    case "Btu":
-      let BtuValue = parseFloat(document.getElementById("BtuInput").value);
-      if (!isNaN(BtuValue)) {
-        document.getElementById("kWInput").value = (BtuValue / kWtoBtu).toFixed(
-          3
-        );
-        document.getElementById("tonInput").value = (
-          (BtuValue / kWtoBtu) *
-          kWtoTon
-        ).toFixed(3);
-        document.getElementById("HPInput").value = (
-          (BtuValue / kWtoBtu) *
-          kWtoHP
-        ).toFixed(3);
-      }
-      break;
-    case "Ton":
-      let tonValue = parseFloat(document.getElementById("tonInput").value);
-      if (!isNaN(tonValue)) {
-        document.getElementById("kWInput").value = (tonValue / kWtoTon).toFixed(
-          3
-        );
-        document.getElementById("BtuInput").value = Math.round(
-          tonValue * kWtoBtu
-        );
-        document.getElementById("HPInput").value = (tonValue * kWtoHP).toFixed(
-          3
-        );
-      }
-      break;
+
     case "HP":
       let HPValue = parseFloat(document.getElementById("HPInput").value);
       if (!isNaN(HPValue)) {
@@ -63,7 +47,76 @@ function convert(type) {
         ).toFixed(3);
       }
       break;
-    default:
+
+    case "Btu":
+      let BtuValue = parseFloat(document.getElementById("BtuInput").value);
+      if (!isNaN(BtuValue)) {
+        document.getElementById("kWInput").value = (BtuValue / kWtoBtu).toFixed(
+          3
+        );
+        document.getElementById("tonInput").value = (
+          (BtuValue * kWtoTon) /
+          kWtoBtu
+        ).toFixed(3);
+        document.getElementById("HPInput").value = (
+          (BtuValue * kWtoHP) /
+          kWtoBtu
+        ).toFixed(3);
+      }
+      break;
+
+    case "Ton":
+      let tonValue = parseFloat(document.getElementById("tonInput").value);
+      if (!isNaN(tonValue)) {
+        document.getElementById("kWInput").value = (tonValue / kWtoTon).toFixed(
+          3
+        );
+        document.getElementById("BtuInput").value = Math.round(
+          tonValue * kWtoBtu
+        );
+        document.getElementById("HPInput").value = (tonValue * kWtoHP).toFixed(
+          3
+        );
+      }
+      break;
+
+    case "C":
+      let CValue = parseFloat(document.getElementById("CInput").value);
+      if (!isNaN(CValue)) {
+        document.getElementById("FInput").value = CtoF(CValue).toFixed(3);
+        document.getElementById("RInput").value = CtoR(CValue).toFixed(3);
+        document.getElementById("KInput").value = CtoK(CValue).toFixed(3);
+      }
+      break;
+
+    case "F":
+      let FValue = parseFloat(document.getElementById("FInput").value);
+      if (!isNaN(FValue)) {
+        let convertedC = FtoC(FValue);
+        document.getElementById("CInput").value = convertedC.toFixed(3);
+        document.getElementById("RInput").value = CtoR(convertedC).toFixed(3);
+        document.getElementById("KInput").value = CtoK(convertedC).toFixed(3);
+      }
+      break;
+
+    case "R":
+      let RValue = parseFloat(document.getElementById("RInput").value);
+      if (!isNaN(RValue)) {
+        let convertedC = RtoC(RValue);
+        document.getElementById("CInput").value = convertedC.toFixed(3);
+        document.getElementById("FInput").value = CtoF(convertedC).toFixed(3);
+        document.getElementById("KInput").value = CtoK(convertedC).toFixed(3);
+      }
+      break;
+
+    case "K":
+      let KValue = parseFloat(document.getElementById("KInput").value);
+      if (!isNaN(KValue)) {
+        let convertedC = KtoC(KValue);
+        document.getElementById("CInput").value = convertedC.toFixed(3);
+        document.getElementById("FInput").value = CtoF(convertedC).toFixed(3);
+        document.getElementById("RInput").value = CtoR(convertedC).toFixed(3);
+      }
       break;
   }
 }
